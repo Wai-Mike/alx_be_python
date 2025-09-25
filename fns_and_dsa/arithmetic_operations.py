d78dc904243a19e3c7fd744bc5c7cfeabca34c1f
Wai Mike <waimichael35@gmail.com>
2025-09-25 10:21:52 +0200

    functions and data structures file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,27 @@
+def perform_operation(num1, num2, operation):
+    """
+    Performs basic arithmetic operations on two numbers.
+    
+    Args:
+        num1 (float): First number
+        num2 (float): Second number
+        operation (str): Operation to perform ('add', 'subtract', 'multiply', 'divide')
+    
+    Returns:
+        float: Result of the arithmetic operation
+        str: Error message for division by zero
+    """
+    operation = operation.lower().strip()
+    
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
